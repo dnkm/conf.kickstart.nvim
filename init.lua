a65f0ea4fe690471a10f66d4b96c37cc44d97a6e
Daniel Kim <daniel@btreecode.com>
2026-01-07 16:38:56 -0800

    treesitter thingy fix (uses master for now)

--- init.lua
+++ init.lua
@@ -942,7 +942,7 @@ require('lazy').setup({
     'nvim-treesitter/nvim-treesitter',
     branch = 'master',
     build = ':TSUpdate',
-    main = 'nvim-treesitter.config', -- Sets main module to use for opts
+    main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
       ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
